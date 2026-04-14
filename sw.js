896c2866aa02280704c1f9f76e2909ca84b59f0f
calebghall <calebghall@gmail.com>
2026-04-14 00:24:49 -0500

    Update sw.js to allow for forcing cache refresh

--- sw.js
+++ sw.js
@@ -1,7 +1,44 @@
-const CACHE_NAME = 'astrolabe-offline-v1';
-      self.addEventListener('install', event => {
-          event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.add('./')));
-      });
-      self.addEventListener('fetch', event => {
-          event.respondWith(caches.match(event.request).then(res => res || fetch(event.request)));
-      });
+const CACHE_NAME = 's&t-clock-v1'; 
+const CACHE_ASSETS = [
+    './',
+    'index.html',
+    'js/lib/tz-lookup/tz.js' 
+];
+
+// SW saves cached version
+self.addEventListener('install', event => {
+    // Forces the new SW to become active immediately
+    self.skipWaiting(); 
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+              return cache.addAll(CACHE_ASSETS);
+        })
+    );
+});
+
+// Clean up old versions
+self.addEventListener('activate', event => {
+    event.waitUntil(self.clients.claim());
+    event.waitUntil(
+        caches.keys().then(keys => Promise.all(
+            keys.map(key => {
+                if (key !== CACHE_NAME) return caches.delete(key);
+            })
+        ))
+    );
+});
+
+// Sticks with cached version / Offline support
+// Downloads new site ONLY when version changes
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(cachedResponse => {
+            // If it's in the cache, return it immediately (NO network request)
+            if (cachedResponse) {
+                return cachedResponse;
+            }
+            // Otherwise, go to the network
+            return fetch(event.request);
+        })
+    );
+});
